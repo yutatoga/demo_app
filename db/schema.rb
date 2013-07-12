2d123ffa19b82f896fb5aaaacd3ac9e7b051745f
yutatoga <yuta.toga@gmail.com>
2013-07-12 12:24:03 +0900

    Finish demo app

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130710110104) do
+ActiveRecord::Schema.define(version: 20130712030919) do
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
